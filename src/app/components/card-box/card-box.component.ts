b8145915403068d5c83b2b86948c8724852091a2
Eneas Junior <eneas.silva.jr@gmail.com>
2023-07-25 01:41:23 -0300

    Implementing dynamic names and account info as well onInit hook

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/accountDataModel';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
-  styleUrls: ['./card-box.component.css']
+  styleUrls: ['./card-box.component.css'],
 })
-export class CardBoxComponent {
-
+export class CardBoxComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  accountData: AccountDataModel = {
+    name: 'Darth Vader',
+    account: {
+      agency: '0001',
+      number: '00.000000-0',
+    },
+    card: {
+      limit: 0,
+      number: '0000',
+    },
+  };
 }
