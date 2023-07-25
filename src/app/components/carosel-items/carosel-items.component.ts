179d28045d91f9d2371a1521f4ea98fc2f517bc5
Eneas Junior <eneas.silva.jr@gmail.com>
2023-07-24 22:57:54 -0300

    Replacing source files

---
+++ src/app/components/carosel-items/carosel-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carosel-items',
+  templateUrl: './carosel-items.component.html',
+  styleUrls: ['./carosel-items.component.css']
+})
+export class CaroselItemsComponent {
+
+}
